8ee4ac6afd95cc6902f46e5fe532c1489f22cb47
Coenraad Stijne <coenraad.stijne@gmail.com>
2017-11-19 19:50:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# scope-jump README
+
+* `column-jump.jumpUp`: Navigate cursor up to next non-empty column
+* `column-jump.jumpDown`: Navigate cursor down to next non-empty column
---
+++ src/extension.ts
@@ -0,0 +1,44 @@
+'use strict';
+import * as vscode from 'vscode';
+import { Range, Position } from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    context.subscriptions.push(vscode.commands.registerCommand('column-jump.jumpDown', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) { return; }
+        const cursorPos = editor.selection.active;
+        const lineCount = editor.document.lineCount;
+        for (let i = cursorPos.line + 1; i < lineCount; i++) {
+            if (jump(editor, cursorPos, i)) {
+                return;
+            }
+        }
+    }));
+
+    context.subscriptions.push(vscode.commands.registerCommand('column-jump.jumpUp', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) { return; }
+        const cursorPos = editor.selection.active;
+
+        for (let i = cursorPos.line - 1; i > 0; i--) {
+            if (jump(editor, cursorPos, i)) {
+                return;
+            }
+        }
+    }));
+}
+
+function jump(editor: vscode.TextEditor, cursorPos: Position, i: number): boolean {
+    const line = editor.document.lineAt(i);
+    if (!line.isEmptyOrWhitespace && line.firstNonWhitespaceCharacterIndex <= cursorPos.character) {
+        const newPos = new Position(i, cursorPos.character);
+        editor.revealRange(new Range(newPos, newPos));
+        var newSelection = new vscode.Selection(newPos, newPos);
+        editor.selection = newSelection;
+        return true;
+    }
+    return false;
+}
+
+export function deactivate() {
+}
